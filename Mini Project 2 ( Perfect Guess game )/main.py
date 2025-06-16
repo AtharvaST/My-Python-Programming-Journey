5629a629d45a396afb97be83a2971d1baeeff933
AtharvaST <teliatharva@gmail.com>
2025-06-17 03:32:29 +0530

    Perfect Guess game

---
+++ Mini Project 2 ( Perfect Guess game )/main.py
@@ -0,0 +1,19 @@
+import random
+
+n = random.randint(1 , 100)
+
+a = -1
+guesses = 1
+while ( a != n ) :
+    guesses+=1
+    a = int(input("Guess a number : "))
+    if(a>n):
+        print("Lower Number please")
+        # guesses+=1
+
+    elif(a<n) :
+        print("Higher number please")
+        # guesses+=1
+
+
+print(f"You have guessed the number {n} in {guesses} attempts.")
